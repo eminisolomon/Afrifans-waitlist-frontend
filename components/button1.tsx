85b66986f409660e47b80dd44412c295d7a058a5
devrealsolomon <>
2023-04-18 05:41:02 +0000

    Pushed from Locofy Builder

--- components/button1.tsx
+++ components/button1.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import { memo } from "react";
 
-const Button1: NextPage = memo(() => {
+const Button: NextPage = memo(() => {
   return (
     <div className="absolute top-[577px] left-[5%] md:left-auto md:right-[5%] rounded-xl bg-primarycolor-400 w-[90%] md:w-[308px] h-[70px] overflow-hidden flex flex-col py-[30px] px-[6%] box-border items-center justify-center text-2xl md:text-5xl text-white font-paragraph-text">
       <div className="relative font-semibold">Join the waitlist</div>
@@ -9,5 +9,4 @@ const Button1: NextPage = memo(() => {
   );
 });
 
-export default Button1;
-
+export default Button;
